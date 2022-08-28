ffb21b4bf99ebfcde5a52ae34013e37dea88fd1e
damodarkolar <damodarkolar@gmail.com>
2022-08-29 00:19:08 +0530

    add

--- day-4(express_middleware)/logger_middleware/index.js
+++ day-4(express_middleware)/logger_middleware/index.js
@@ -3,7 +3,7 @@ const morgan=require("morgan")
 
 const app=express();
 
-app.use(morgan(':method :status :[content-length] :time taken :date :http version :url accesed :newline at the end'))
+app.use(morgan(':method :status :[content-length] :time taken :date :http version :url accesed :newline at the :end'))
 
 app.get("/", (req, res)=>{
     res.send("logged ");
